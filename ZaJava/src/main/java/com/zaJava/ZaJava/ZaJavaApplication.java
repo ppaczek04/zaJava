77357d5d55e35d01be9609b8be272c19c883be7f
Piotr Paczek <ppaczek1@o2.pl>
2024-08-05 19:30:31 +0200

    spring_proj

---
+++ ZaJava/src/main/java/com/zaJava/ZaJava/ZaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.zaJava.ZaJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaJavaApplication.class, args);
+	}
+
+}
---
+++ ZaJava/src/test/java/com/zaJava/ZaJava/ZaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaJava.ZaJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
