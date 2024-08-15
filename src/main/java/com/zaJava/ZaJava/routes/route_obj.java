726d7fba939188067190d555e38f67ce08fd3422
Piotr Paczek <ppaczek1@o2.pl>
2024-08-15 12:10:25 +0200

    basic_point_route_class

---
+++ src/main/java/com/zaJava/ZaJava/routes/route_obj.java
@@ -0,0 +1,8 @@
+package com.zaJava.ZaJava.routes;
+
+public class route_obj {
+    private double distance;
+    private double time;
+    private MapPoint startPoint;
+    private MapPoint endPoint;
+}
